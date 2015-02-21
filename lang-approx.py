8571d78894734af5e0f2e9a8ae53df5d59c4e823
Anthony Chiu <secaucus@gmail.com>
2015-02-21 22:57:04 +0000

    Input now given as command line arguments.

--- lang-approx.py
+++ lang-approx.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 import fileinput
+import json
+import argparse
 import random
 import re
 import string
 import sys
-import json
 
 from collections import defaultdict
 
 class ChainMode:
-    LETTERS = 0
+    CHARS = 0
     WORDS = 1
 
 class OutputMode:
-    LETTERS = 0
+    CHARS = 0
     WORDS = 1
     SENTENCES = 2
 
@@ -34,6 +35,7 @@ class LanguageGenerator:
         self.chainMode = chainMode
         self.mc = autovivify(chainLength + 1, int)
         self.build_markov_chain(inFile)
+        inFile.close()
 
     def get_restart_msg_string(self):
         if self.chainLength == 1:
@@ -48,13 +50,13 @@ class LanguageGenerator:
             that we are at the start of a sentence. In the case of characters, 
             this (roughly!) corresponds to the last 2 characters being ". ".
         """
-        if self.chainMode == ChainMode.LETTERS:
+        if self.chainMode == ChainMode.CHARS:
             return self.get_restart_msg_string()
         else:
             return [" "]
 
     def get_empty_string(self):
-        if self.chainMode == ChainMode.LETTERS:
+        if self.chainMode == ChainMode.CHARS:
             return ""
         elif self.chainMode == ChainMode.WORDS:
             return []
@@ -68,13 +70,13 @@ class LanguageGenerator:
             words.
         """
         tidiedSentence = string.strip(l) + " "
-        if self.chainMode == ChainMode.LETTERS:
+        if self.chainMode == ChainMode.CHARS:
             return tidiedSentence
         elif self.chainMode == ChainMode.WORDS:
             return self.wordify_line(tidiedSentence)
 
     def get_blank_line(self):
-        if self.chainMode == ChainMode.LETTERS:
+        if self.chainMode == ChainMode.CHARS:
             return " "
         elif self.chainMode == ChainMode.WORDS:
             return [" "]
@@ -87,7 +89,7 @@ class LanguageGenerator:
             Join strings together in the appropriate way (depending on 
             self.chainMode.
         """
-        if self.chainMode == ChainMode.LETTERS:
+        if self.chainMode == ChainMode.CHARS:
             return prevString + currentString
         elif self.chainMode == ChainMode.WORDS:
             return prevString + [currentString]
@@ -97,24 +99,23 @@ class LanguageGenerator:
         nextString = self.get_empty_string()
         currentChain = self.mc
         lastLine = self.get_blank_line()
-        with open(inFile) as f:
-            for line in f:
-                sLine = self.tidy_sentence(line)
-                if not self.is_blank(lastLine) or not self.is_blank(sLine):
-                    for i in xrange(len(sLine)):
-                        if len(lastString) < self.chainLength:
-                            """ At the start of the input text. """
-                            lastString = self.joined_strings(lastString,
-                                                             sLine[i])
-                        else:
-                            currentChain = self.mc
-                            for c in lastString:
-                                currentChain = currentChain[c]
-                            nextString = sLine[i]
-                            currentChain[nextString] += 1
-                            lastString = self.joined_strings(lastString[1:],
-                                                             nextString)
-                lastLine = sLine
+        for line in inFile:
+            sLine = self.tidy_sentence(line)
+            if not self.is_blank(lastLine) or not self.is_blank(sLine):
+                for i in xrange(len(sLine)):
+                    if len(lastString) < self.chainLength:
+                        """ At the start of the input text. """
+                        lastString = self.joined_strings(lastString,
+                                                         sLine[i])
+                    else:
+                        currentChain = self.mc
+                        for c in lastString:
+                            currentChain = currentChain[c]
+                        nextString = sLine[i]
+                        currentChain[nextString] += 1
+                        lastString = self.joined_strings(lastString[1:],
+                                                         nextString)
+            lastLine = sLine
 
     def dump(self, s=None):
         mc = self.mc
@@ -129,7 +130,7 @@ class LanguageGenerator:
             set of all possible strings following x (weighted according to its 
             empirical frequency).
         """
-        if self.chainMode == ChainMode.LETTERS:
+        if self.chainMode == ChainMode.CHARS:
             return [x + s for s in nextLot]
         elif self.chainMode == ChainMode.WORDS:
             return [[x] + s for s in nextLot]
@@ -140,7 +141,7 @@ class LanguageGenerator:
             end of a given sequence in the Markov chain.  mc should be the 
             Markov chain at the final step.
         """
-        if self.chainMode == ChainMode.LETTERS:
+        if self.chainMode == ChainMode.CHARS:
             return [s for s in mc for t in xrange(mc[s])]
         elif self.chainMode == ChainMode.WORDS:
             return [[s] for s in mc for t in xrange(mc[s])]
@@ -184,13 +185,13 @@ class LanguageGenerator:
         return randList
 
     def at_word_boundary(self, lastString):
-        if self.chainMode == ChainMode.LETTERS:
+        if self.chainMode == ChainMode.CHARS:
             return lastString[:1] == " "
         elif self.chainMode == ChainMode.WORDS:
             return True
        
     def at_end_of_sentence(self, lastString):
-        if self.chainMode == ChainMode.LETTERS:
+        if self.chainMode == ChainMode.CHARS:
             if self.chainLength >= 2:
                 return lastString[-2:] == "  "
             elif self.chainMode == 1:
@@ -199,7 +200,7 @@ class LanguageGenerator:
             return lastString[:1] == " "
 
     def list_to_string(self, l):
-        if self.chainMode == ChainMode.LETTERS:
+        if self.chainMode == ChainMode.CHARS:
             return l
         elif self.chainMode == ChainMode.WORDS:
             return "".join(l).rstrip()
@@ -217,7 +218,7 @@ class LanguageGenerator:
             msg += nextString
             lastString = lastString[1:] + nextString
                 
-            if (mode == OutputMode.LETTERS
+            if (mode == OutputMode.CHARS
                     or (mode != OutputMode.SENTENCES
                         and self.chainMode == ChainMode.WORDS)
                     or (mode == OutputMode.WORDS
@@ -234,10 +235,39 @@ class LanguageGenerator:
 def main(argv=None):
     if argv is None:
         argv = sys.argv
-	
+    
+    p = argparse.ArgumentParser(description="Take some text, create a Markov \
+            chain (of chars or words), and use the Markov chain to random \
+            generate words and sentences.")
+    
+    # input file
+    p.add_argument("-i", "--input-file", nargs="?",
+            type=argparse.FileType("r"), default=sys.stdin, metavar="FILE",
+            help="the FILE from which to create the Markov chain (default: \
+            stdin)")
+    # chain mode
+    p.add_argument("-c", "--chars", dest="chain_chars", action="store_true",
+            help="create a Markov chain of characters (default)")
+    p.add_argument("-w", "--words", dest="chain_words", action="store_true",
+            help="create a Markov chain of words")
+    # chain length
+    p.add_argument("-l", "--chain-length", nargs="?", type=int,
+            default=1, metavar="LENGTH",
+            help="the LENGTH of the Markov chain")
+    
+    args = p.parse_args()
+    
+    # The chainMode will default to chars unless -w is the *only* argument.
+    if args.chain_words and not args.chain_chars:
+        chainMode = ChainMode.WORDS
+        print "words"
+    else:
+        chainMode = ChainMode.CHARS
+        print "chars"
+    
     # Build the Markov chain
-    mc = LanguageGenerator(argv[1], 3, ChainMode.WORDS)
-    mc.print_random_message(500, OutputMode.WORDS)
+    mc = LanguageGenerator(args.input_file, args.chain_length, chainMode)
+    mc.print_random_message(200, OutputMode.WORDS)
     
 if __name__ == "__main__":
     sys.exit(main())
